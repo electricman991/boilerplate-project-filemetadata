9c46fea2efa1deb96dc6eb2bad5e30dd5d740243
electricman991 <braidensommer@yahoo.com>
2024-06-28 01:22:57 -0700

    Finished challenge

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
 
+app.use(express.json())
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
 
+    console.log(req.file)
+    const {originalname, mimetype, size} = req.file
+
+    res.json({filename: originalname, fileType: mimetype, fileSize: size})
+})
 
 
 const port = process.env.PORT || 3000;
